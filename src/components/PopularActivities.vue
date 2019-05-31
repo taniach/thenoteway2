b2d5840bbd59d4812cdd225ff1a1ff17d83563d2
Tania <tchatterjee5@gmail.com>
2019-05-31 17:33:35 +0800

    Popular activities

---
+++ src/components/PopularActivities.vue
@@ -0,0 +1,172 @@
+<template>
+    <v-container grid-list-lg>
+        <v-layout row>
+            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">
+                <img src="@/assets/trending.png" id="popular" />
+                Popular Activities
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap class="row-cards">
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img :src="require('@/assets/Corporate-Team-Bonding-Session-1024x768.jpg')" height="300px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text"></span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Art Jamming with a Twist</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img :src="require('@/assets/clay.jpeg')" height="300px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text"></span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Clay Workshop</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img :src="require('@/assets/Terrarium-300x225.jpeg')" height="300px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text"></span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Terrarium Workshop</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap class="row-cards">
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img :src="require('@/assets/Sand-Art-300x214.jpeg')" height="300px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text"></span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Sand Art Workshop</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img :src="require('@/assets/pexels-photo-1559046-copy.jpeg')" height="300px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text"></span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Brush Lettering Workshop</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img :src="require('@/assets/paper-quilling-flower-2-726x1024.jpeg')" height="300px">
+                        <v-container fill-height fluid>
+                            <v-layout fill-height>
+                                <v-flex xs12 align-end flexbox>
+                                    <span class="headline white--text"></span>
+                                </v-flex>
+                            </v-layout>
+                        </v-container>
+                    </v-img>
+                    <v-card-title primary-title>
+                        <div>
+                            <h3 class="headline mb-0">Paper Quilling Workshop</h3>
+                            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam mauris felis, varius rutrum massa a, dignissim ornare dui. Cras eget velit eu dui tristique lobortis sit amet vel tellus.
+                            </div>
+                        </div>
+                    </v-card-title>
+                </v-card>
+            </v-flex>
+        </v-layout>
+        <div class="text-xs-center">
+            <v-btn large id="see-more" dark>SEE MORE</v-btn>
+        </div>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'PopularActivities'
+};
+</script>
+
+<style scoped>
+.display-1 {
+    color: #FF00AC;
+}
+
+#see-more {
+    background-color: #FF00AC;
+    margin-bottom: 30px;
+}
+
+#popular {
+    max-height: 40px;
+}
+
+.mb-0 {
+    padding-bottom: 10px;
+}
+
+.row-cards {
+    padding-bottom: 30px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <span>
+        <popular-activities></popular-activities>
         <home-hero></home-hero>
         <home-details></home-details>
         <home-plans></home-plans>
@@ -10,10 +11,12 @@
 import HomeHero from '@/components/HomeHero';
 import HomeDetails from '@/components/HomeDetails';
 import HomePlans from '@/components/HomePlans';
+import PopularActivities from '@/components/PopularActivities';
 
 export default {
     name: 'home',
     components: {
+        PopularActivities,
         HomeHero,
         HomeDetails,
         HomePlans
