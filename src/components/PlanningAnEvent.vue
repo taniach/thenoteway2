b1699c57e4f6f1c23c3fe11a105bac279947704a
Tania <tchatterjee5@gmail.com>
2019-06-03 14:34:22 +0800

    Planning an event

---
+++ src/components/PlanningAnEvent.vue
@@ -0,0 +1,81 @@
+<template>
+    <v-container fluid grid-list-lg>
+        <v-layout row>
+            <v-flex xs12 class="text-xs-center display-1 font-weight-black my-5">
+                <img src="@/assets/multiple-users-silhouette.png" id="users" />
+                Planning An Event
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap class="row-cards" id="row-of-three">
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img :src="require('@/assets/corporate.jpg')" height="300px">
+                    </v-img>
+                </v-card>
+                <div class="text-xs-center">
+                    <v-btn large class="see-more" dark>CORPORATE EVENTS</v-btn>
+                </div>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img :src="require('@/assets/parties.jpg')" height="300px">
+                    </v-img>
+                </v-card>
+                <div class="text-xs-center">
+                    <v-btn large class="see-more" dark>PARTIES</v-btn>
+                </div>
+            </v-flex>
+
+            <v-flex xs12 sm12 md4>
+                <v-card>
+                    <v-img :src="require('@/assets/entrance.jpg')" height="300px">
+                    </v-img>
+                </v-card>
+                <div class="text-xs-center">
+                    <v-btn large class="see-more" dark>JUST THE VENUE</v-btn>
+                </div>
+            </v-flex>
+        </v-layout>
+        
+        
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'PlanningAnEvent'
+};
+</script>
+
+<style scoped>
+.display-1 {
+    color: black;
+}
+
+.see-more {
+    margin-top: 30px;
+    margin-bottom: 30px;
+}
+
+#users {
+    max-height: 40px;
+}
+
+.mb-0 {
+    padding-bottom: 10px;
+}
+
+.row-cards {
+    padding-bottom: 30px;
+}
+
+#row-of-three {
+    margin-left: 80px;
+    margin-right: 80px;
+}
+
+.container {
+    background-color: #e9e9e9;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
     <span>
         <popular-activities></popular-activities>
+        <planning-an-event></planning-an-event>
     </span>
 </template>
 
 <script>
 import PopularActivities from '@/components/PopularActivities';
+import PlanningAnEvent from '@/components/PlanningAnEvent';
 
 export default {
     name: 'home',
     components: {
-        PopularActivities
+        PopularActivities,
+        PlanningAnEvent
     }
 };
 </script>
